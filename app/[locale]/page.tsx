8119c4a7d27aee0b789eace8a0a409ebc48a8016
KarolG99 <karolus2212@gmail.com>
2024-01-07 16:32:29 +0100

    Temporary change revalidation time

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -7,7 +7,8 @@ import supabase from "../services/supabase";
 import { ParamsType } from "../types";
 import RestaurantsCarousel from "../ui/RestaurantsCarousel";
 
-export const revalidate = 21600;
+// export const revalidate = 21600;
+export const revalidate = 60;
 
 export default async function Home({ params: { locale } }: ParamsType) {
   const dictionary = getDictionary(locale);
